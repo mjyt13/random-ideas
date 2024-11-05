c6aae79113db81d5432fead403c36329bd331015
Maxim <116669212+mjyt13@users.noreply.github.com>
2024-11-05 13:37:05 +0500

    Update DiscreteStochastic.cs again
    automatization of modeling

--- DiscreteStochastic.cs
+++ DiscreteStochastic.cs
@@ -10,20 +10,24 @@ class Program
         Console.Write("Введите время обработки заказа (в минутах): ");
         double processingTime = double.Parse(Console.ReadLine());
         // Ввод среднего времени появления следующего покупателя
-        Console.Write("Введите среднее время появления следующего покупателя (в минутах): ");
+        Console.Write("Введите среднее количество покупателей в минуту: ");
         double averageArrivalTime = double.Parse(Console.ReadLine());
         // Ввод количества итераций (длительность моделирования в минутах)
         Console.Write("Введите количество итераций (моделируемое время в минутах): ");
         double totalSimulationTime = double.Parse(Console.ReadLine());
-        var results = SimulateCheckout(numberOfCashiers, processingTime, averageArrivalTime,
-totalSimulationTime);
         // Вывод результатов
-        Console.WriteLine("\nСреднее количество обслуженных клиентов на каждую кассу:");
-        for (int i = 0; i < numberOfCashiers; i++)
+        for (int j = 0; j < 100; j++)
         {
-            Console.WriteLine($"Касса {i + 1}: {results.Item1[i]:F7}");
+            var results = SimulateCheckout(numberOfCashiers, processingTime, averageArrivalTime,
+totalSimulationTime);
+            Console.WriteLine("\nСреднее количество обслуженных клиентов на каждую кассу:");
+            for (int i = 0; i < numberOfCashiers; i++)
+            {
+                Console.WriteLine($"Касса {i + 1}: {results.Item1[i]}");
+            }
+            Console.WriteLine($"Количество потерянных клиентов: {results.Item2}");
+
         }
-        Console.WriteLine($"Количество потерянных клиентов: {results.Item2}");
     }
     static (double[], double) SimulateCheckout(int nCashiers, double processingTime, double
 avgInterarrivalTime, double totalSimulationTime)
@@ -33,11 +37,13 @@ avgInterarrivalTime, double totalSimulationTime)
         double[] cashierAvailableAt = new double[nCashiers];  // Когда каждая касса будет свободна
         double currentTime = 0;  // Текущее время симуляции
         double nextCustomerArrival = GenerateExponentialRandom(avgInterarrivalTime);  // Первое время появления покупателя
-
-        double daysNumber = 1500;
+        double avgLostCustomers = 0;
+        double[] averageServiced = new double[nCashiers];
+        double daysNumber = 1000;
         for(int j = 0; j < daysNumber; j++)
         {
             currentTime = 0;
+
             while (currentTime < totalSimulationTime)
             {
                 // Если следующий клиент приходит после завершения текущей симуляции
@@ -68,15 +74,14 @@ avgInterarrivalTime, double totalSimulationTime)
                 // Генерируем время прихода следующего покупателя
                 nextCustomerArrival += GenerateExponentialRandom(avgInterarrivalTime);
             }
-        }
 
-        // Подсчет средних значений для каждой кассы
-        double[] averageServiced = new double[nCashiers];
-        for (int i = 0; i < nCashiers; i++)
-        {
-            averageServiced[i] = (double)serviced[i]/daysNumber;
+            // Подсчет средних значений для каждой кассы
+            for (int i = 0; i < nCashiers; i++)
+            {
+                averageServiced[i] += (double)serviced[i] / daysNumber;
+            }
+            avgLostCustomers += (double)lostCustomers / daysNumber;
         }
-        double avgLostCustomers = (double)lostCustomers/daysNumber; 
         return (averageServiced, avgLostCustomers);
     }
     // Генерация случайного времени по экспоненциальному закону
@@ -84,6 +89,6 @@ avgInterarrivalTime, double totalSimulationTime)
     {
         Random rand = new();
         double u = rand.NextDouble();
-        return -Math.Log(1 - u) * a;
+        return -Math.Log(1 - u) / a;
     }
 }
